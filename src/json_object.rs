bf4345e03ec5ace1667f74091e92d36a1bd9c3ae
alleysark <dev.alleysark@gmail.com>
2018-03-08 02:03:40 +0900

    impl Clone and From traits for convenient

--- src/json_object.rs
+++ src/json_object.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::convert::From;
 
 #[derive(Debug)]
 pub struct JsonObject {
@@ -16,6 +17,7 @@ pub enum JsonValue {
     AsNull,
 }
 
+// ----- impl JsonObject methods -----
 impl JsonObject {
     pub fn new() -> JsonObject {
         JsonObject {
@@ -37,6 +39,20 @@ impl JsonObject {
     }
 }
 
+// ----- impl JsonObject Clone trait -----
+impl Clone for JsonObject {
+    fn clone(&self) -> JsonObject {
+        JsonObject {
+            collection: self.collection.clone()
+        }
+    }
+
+    fn clone_from(&mut self, source: &Self) {
+        self.collection.clone_from(&source.collection);
+    }
+}
+
+// ----- define JsonObject construction macro -----
 macro_rules! json_obj (
     { $($key:expr => $value:expr), * } => {{
         let mut obj = JsonObject::new();
@@ -46,3 +62,60 @@ macro_rules! json_obj (
         obj
     }};
 );
+
+// ----- impl JsonValue Clone trait -----
+impl Clone for JsonValue {
+    fn clone(&self) -> JsonValue {
+        match *self {
+            JsonValue::AsStr(ref s) => JsonValue::AsStr(s.clone()),
+            JsonValue::AsInt(i) => JsonValue::AsInt(i),
+            JsonValue::AsFloat(f) => JsonValue::AsFloat(f),
+            JsonValue::AsObject(ref obj) => JsonValue::AsObject(obj.clone()),
+            JsonValue::AsArray(ref arr) => JsonValue::AsArray(arr.clone()),
+            JsonValue::AsBool(b) => JsonValue::AsBool(b),
+            JsonValue::AsNull => JsonValue::AsNull,
+        }
+    }
+}
+
+// ----- impl JsonValue From trait -----
+impl<'a> From<&'a str> for JsonValue {
+    fn from(s: &'a str) -> Self {
+        JsonValue::AsStr(String::from(s))
+    }
+}
+impl From<i32> for JsonValue {
+    fn from(i: i32) -> Self {
+        JsonValue::AsInt(i)
+    }
+}
+impl From<f64> for JsonValue {
+    fn from(f: f64) -> Self {
+        JsonValue::AsFloat(f)
+    }
+}
+impl<'a> From<&'a JsonObject> for JsonValue {
+    fn from(obj: &'a JsonObject) -> Self {
+        JsonValue::AsObject(obj.clone())
+    }
+}
+impl From<JsonObject> for JsonValue {
+    fn from(obj: JsonObject) -> Self {
+        JsonValue::AsObject(obj)
+    }
+}
+impl<'a> From<&'a Vec<JsonValue>> for JsonValue {
+    fn from(arr: &'a Vec<JsonValue>) -> Self {
+        JsonValue::AsArray(arr.clone())
+    }
+}
+impl From<Vec<JsonValue>> for JsonValue {
+    fn from(arr: Vec<JsonValue>) -> Self {
+        JsonValue::AsArray(arr)
+    }
+}
+impl From<bool> for JsonValue {
+    fn from(b: bool) -> Self {
+        JsonValue::AsBool(b)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,33 @@ fn main() {
     use json_object::*;
 
     let mut inner_obj = JsonObject::new();
-    inner_obj.insert("field_1_1", JsonValue::AsInt(10));
-    inner_obj.insert("field_1_2", JsonValue::AsFloat(3.14));
+    inner_obj.insert("field_1_1", JsonValue::from(10));
+    inner_obj.insert("field_1_2", JsonValue::from(3.14));
 
-    let mut doc = JsonObject::new();
-    doc.insert("field_0", JsonValue::AsInt(10));
-    doc.insert("inner_obj", JsonValue::AsObject(inner_obj));
+    let mut obj = JsonObject::new();
+    obj.insert("field_0", JsonValue::from(10));
+    obj.insert("inner_obj", JsonValue::from(inner_obj));
 
-    println!("{:#?}", doc);
-    println!("{:#?}", doc.get("inner_obj"));
-    println!("length of doc: {}", doc.len());
+    println!("obj: {:#?}", obj);
+    println!("inner_obj: {:#?}", obj.get("inner_obj"));
+    println!("length of obj: {}", obj.len());
+    println!("");
 
     // using macro
-    let mut obj = json_obj!{
-        "age" => JsonValue::AsInt(28),
-        "favorites" => JsonValue::AsArray(vec![
-            JsonValue::AsObject(json_obj!{
-                "type" => JsonValue::AsStr(String::from("color")),
-                "target" => JsonValue::AsStr(String::from("violet"))
+    let obj = json_obj!{
+        "age" => JsonValue::from(28),
+        "favorites" => JsonValue::from(vec![
+            JsonValue::from(json_obj!{
+                "type" => JsonValue::from("color"),
+                "target" => JsonValue::from("violet")
             }),
-            JsonValue::AsFloat(0.1),
-            JsonValue::AsFloat(0.2),
-            JsonValue::AsFloat(0.3),
+            JsonValue::from(0.1),
+            JsonValue::from(0.2),
+            JsonValue::from(0.3),
         ])
     };
-    println!("{:#?}", obj);
+    println!("obj: {:#?}", obj);
+
+    let cloned_obj = obj.clone();
+    println!("cloned_obj: {:#?}", cloned_obj);
 }
