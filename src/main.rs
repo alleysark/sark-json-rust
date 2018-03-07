af33117195906b81e8172989a9b7cbba586764d1
alleysark <dev.alleysark@gmail.com>
2018-03-07 22:50:24 +0900

    add very basic json structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+
+#[derive(Debug)]
+struct JSonObject {
+    name: String,
+    value: Box<JSonValue>,
+}
+
+#[derive(Debug)]
+enum JSonValue {
+    as_str(String),
+    as_int(i32),
+    as_float(f64),
+    as_obj(Box<JSonObject>),
+    as_arr(Vec<JSonValue>),
+    as_bool(bool),
+    as_null,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let obj = JSonObject {
+        name: String::from("field_name"),
+        value: Box::new(JSonValue::as_int(10)),
+    };
+
+    println!("{:?}", obj);
 }
