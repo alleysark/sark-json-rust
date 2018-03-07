0093846163d8a1a7778ab6fb4c70ffd38b43dfde
alleysark <dev.alleysark@gmail.com>
2018-03-08 00:15:49 +0900

    impl basic functionality of JsonObject

--- src/json_object.rs
+++ src/json_object.rs
@@ -15,3 +15,18 @@ pub enum JsonValue {
     AsBool(bool),
     AsNull,
 }
+
+impl JsonObject {
+    pub fn get(&self, key: &str) -> Option<&JsonValue> {
+        self.collection.get(key)
+    }
+
+    pub fn len(&self) -> usize {
+        self.collection.len()
+    }
+
+    // if name exists, the value is updated, and the old value is returned (like HashMap of std::collections)
+    pub fn insert(&mut self, name: &str, val: JsonValue) -> Option<JsonValue> {
+        self.collection.insert(String::from(name), val)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,16 @@ fn main() {
     let mut inner_obj = JsonObject {
         collection: HashMap::new()
     };
-    inner_obj.collection.insert(String::from("field_1_1"), JsonValue::AsInt(10));
-    inner_obj.collection.insert(String::from("field_1_2"), JsonValue::AsFloat(3.14));
+    inner_obj.insert("field_1_1", JsonValue::AsInt(10));
+    inner_obj.insert("field_1_2", JsonValue::AsFloat(3.14));
 
     let mut doc = JsonObject {
         collection: HashMap::new()
     };
-    doc.collection.insert(String::from("field_0"), JsonValue::AsInt(10));
-    doc.collection.insert(String::from("inner_obj"), JsonValue::AsObject(inner_obj));
+    doc.insert("field_0", JsonValue::AsInt(10));
+    doc.insert("inner_obj", JsonValue::AsObject(inner_obj));
 
     println!("{:#?}", doc);
+    println!("{:#?}", doc.get("inner_obj"));
+    println!("length of doc: {}", doc.len());
 }
